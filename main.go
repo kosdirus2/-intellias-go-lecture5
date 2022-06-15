3d5b526a0a7be2a1f4d1dc793fd399ff7ec85311
Ivan <kos.dirus@gmail.com>
2022-06-15 10:55:11 +0200

    fixed magic numbers

--- main.go
+++ main.go
@@ -10,14 +10,22 @@ func main() {
 	)
 
 	// 1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?
-	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?", applePrice*9+pearPrice*8, "грн.")
+	priceFor9Apples := applePrice * 9
+	priceFor8Pears := pearPrice * 8
+	priceFor9ApplesAnd8Pears := priceFor9Apples + priceFor8Pears
+	fmt.Println("1. Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?", priceFor9ApplesAnd8Pears, "грн.")
 
 	// 2. Скільки груш ми можемо купити?
-	fmt.Println("2. Скільки груш ми можемо купити?", int(totalBudget/pearPrice))
+	maxAmountOfPearsToBuyWithCurrentBudget := int(totalBudget / pearPrice)
+	fmt.Println("2. Скільки груш ми можемо купити?", maxAmountOfPearsToBuyWithCurrentBudget)
 
 	// 3. Скільки яблук ми можемо купити?
-	fmt.Println("3. Скільки яблук ми можемо купити?", int(totalBudget/applePrice))
+	maxAmountOfApplesToBuyWithCurrentBudget := int(totalBudget / applePrice)
+	fmt.Println("3. Скільки яблук ми можемо купити?", maxAmountOfApplesToBuyWithCurrentBudget)
 
 	// 4. Чи ми можемо купити 2 груші та 2 яблука?
-	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", applePrice*2+pearPrice*2 < totalBudget)
+	priceFor2Apples := applePrice * 2
+	priceFor2Pears := pearPrice * 2
+	priceFor2ApplesAnd2Pears := priceFor2Apples + priceFor2Pears
+	fmt.Println("4. Чи ми можемо купити 2 груші та 2 яблука?", priceFor2ApplesAnd2Pears < totalBudget)
 }
